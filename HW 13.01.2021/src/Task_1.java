55e21544f2fc2d0831757c056a255abeb2d326a4
JustinCooper43 <64972476+JustinCooper43@users.noreply.github.com>
2021-01-15 16:16:06 +0200

    Update Task_1.java

--- HW 13.01.2021/src/Task_1.java
+++ HW 13.01.2021/src/Task_1.java
@@ -21,13 +21,19 @@ public class Task_1 {
         System.out.println("After a = " + a + "  " + "b = " + b);
     }
 
-    public static void changeNumb (int a, int b){
+   public static void changeNumb(int a, int b) {
 
         System.out.println("Before a = " + a + "  " + "b = " + b);
 
-        a = a % b;
-        b = b + a;
-        a = b - a;
+        //Using the XOR operation
+        // a = 3 b = 5
+        // a = 011 in binary
+        // b = 101 in binary
+
+
+        a = b ^ a; // 6 = 3 ^ 5   110 = 011 ^ 101
+        b = b ^ a; // 3 = 5 ^ 6   011 = 101 ^ 110
+        a = b ^ a; // 5 = 3 ^ 6   101 = 011 ^ 110
 
         System.out.println("After a = " + a + "  " + "b = " + b);
 
